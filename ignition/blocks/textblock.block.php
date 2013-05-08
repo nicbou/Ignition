ee702f81e5294704db14001bec82a187bfe96523
Nicolas Bouliane <contact@nicolasbouliane.com>
2013-05-08 18:06:41 -0400

    Clarified comment

--- ignition/blocks/textblock.block.php
+++ ignition/blocks/textblock.block.php
@@ -1,5 +1,5 @@
 <?php
-//Plain text blocks
+//Plain text blocks; escapes HTML and converts newlines to <br> tags
 class TextBlock extends Block{
 	function getContent(){
 		return nl2br(htmlspecialchars($this->bean->content,ENT_QUOTES,'UTF-8'));
